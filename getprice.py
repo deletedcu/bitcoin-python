365ec077e7ca8318fcdaa95b3f325bc1789468ee
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-12-09 14:07:02 +0100

    Using .format() for providing support for older Python versions

--- getprice.py
+++ getprice.py
@@ -6,6 +6,10 @@ if startAppInput == "y":
     request = requests.get("https://bitpay.com/api/rates/usd")
     request = request.json()
 
-    print(f'Current price of one Bitcoin is at: {request["rate"]:.2f}$')
+    # This line will be executed to provide support for older Python 3 versions
+    print("Current price of one Bitcoin is at: {0:.2f}$".format(request["rate"]))
+
+    # You can use this if you are using Python 3.6 or newer.
+    # print(f'Current price of one Bitcoin is at: {request["rate"]:.2f}$')
 else:
     exit(0)
