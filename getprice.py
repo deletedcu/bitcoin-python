b48edd4214e86a304401bb0111fa7a20e9232b90
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2017-12-11 22:39:59 +0100

    Minor changes in project

--- getprice.py
+++ getprice.py
@@ -2,15 +2,21 @@ import requests
 from flask import Flask, jsonify
 from flask import render_template
 
-version = "1.0.0"
+# Variables
+# Version of the app
+version = "1.0.0-12"
 
+# API URL of the API used in this project
 API_url = "https://bitpay.com/api/rates/usd"
 
+# Accessing JSON data
 data = requests.get(API_url)
 data = data.json()
 
+# Price in a single variable
 price = data["rate"]
 price = "{0:.2f}".format(price)
+
 # This line will be executed to provide support for older Python 3 versions
 print("\nConsole output: \nCurrent price of one Bitcoin is at: {0}$".format(price))
 print("API URL: {0}\n".format(API_url))
@@ -19,18 +25,23 @@ print("Flask Output:")
 # You can use this if you are using Python 3.6 or newer.
 # print(f'Current price of one Bitcoin is at: {request["rate"]:.2f}$')
 
+# Creating the app
+
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
-    def runningapp():
-        with app.app_context():
-            return render_template('index.html', version=version, price=price, api=API_url)
+    def home():
+        return render_template('index.html', version=version, price=price, api=API_url)
 
     @app.route("/ping")
     def ping():
         return jsonify(ping='pong')
+
+    @app.route("/raw")
+    def rawoutput():
+        return jsonify(api=API_url, currency=data['name'], currencycode=data['code'], price=price, version=version)
 
     return app
 
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo "Installing now all required packages:"
 pip install -r requirements.txt
